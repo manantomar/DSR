9c8340bbedc2a00b82a7aefe635aa38ac0d1aad7
manan tomar <manan.tomar@gmail.com>
2018-03-06 22:35:29 +0530

    added SR-TD learning

--- SR/SR-TD-Learning.py
+++ SR/SR-TD-Learning.py
@@ -10,13 +10,15 @@ import gym_minigrid
 env = GridworldEnv()
 #env = gym.make(env_id)
 
-EPSILON = 0.3
+EPSILON = 1
 GAMMA = 0.99
 LEARNING_RATE = 0.3
 NUM_EPISODES = 500
 
 # initialize Q
 Q = defaultdict(lambda: np.zeros(env.action_space.n))
+M = np.zeros((env.observation_space.n, env.observation_space.n))
+w = np.zeros(env.observation_space.n)
 
 stats = plotting.EpisodeStats(
         episode_lengths=np.zeros(NUM_EPISODES),
@@ -54,14 +56,27 @@ for i in range(NUM_EPISODES):
         #if done and reward > 0:
         #    next_state = 15
         #env.render()
+        print(M)
 
         stats.episode_rewards[i] += reward
         stats.episode_lengths[i] = steps
 
-        Q_target = reward + GAMMA * Q[next_state][np.argmax(Q[next_state])]
-        Q_error = Q_target - Q[state][action]
+        Q[state][action] = np.dot(M[next_state, :], w)
 
-        Q[state][action] += LEARNING_RATE * Q_error
+        # compute TD error in V
+        V_target = reward + np.dot(M[next_state, :], w)
+        V_error = V_target - np.dot(M[state, :], w)
+
+        # update weights
+        w[:] += LEARNING_RATE * V_error * M[state, :]
+
+        # update M
+        ones = np.zeros(env.observation_space.n)
+        ones[state] = 1
+        M_target = ones + GAMMA * M[next_state,:]
+        M_error = M_target - M[state, :]
+        M[state, :] += LEARNING_RATE * M_error
+
         state = next_state
         steps += 1
 
